bcde4b7e0a251af0d240219c3c2ebce2e85c46ce
kitiwat chanluthin <aurelia@kitiwats-MacBook-Pro.local>
2017-08-15 15:27:04 +0700

    done register service

--- Smack/Utillieties/Constants.swift
+++ Smack/Utillieties/Constants.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success:Bool)->()
+
+//URL Constants
+
+let BASE_URL = "https://chatservice3141.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User DefaultsFoundation
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
