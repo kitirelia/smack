eb8b65486ff4e128b3cdb7a90b6378491a51a10b
kitiwat chanluthin <aurelia@kitiwats-MacBook-Pro.local>
2017-08-18 09:22:33 +0700

    created socketservice retrieve data and check is typing

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -12,11 +12,13 @@ class ChatVC: UIViewController {
 
     //Outlet
     
+    @IBOutlet weak var sendBtn: UIButton!
     @IBOutlet weak var messageTxt: UITextField!
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var channelNameLbl: UILabel!
-    
     @IBOutlet weak var tableView: UITableView!
+    
+    var isTyping:Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +30,8 @@ class ChatVC: UIViewController {
         tableView.estimatedRowHeight = 80
         tableView.rowHeight = UITableViewAutomaticDimension
         
+        sendBtn.isHidden = true
+        
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
@@ -36,6 +40,17 @@ class ChatVC: UIViewController {
         
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange(_:)),name:NOTIF_USER_DATA_DID_CHANGE  ,object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected), name: NOTIF_CHANNEL_SELECTED, object: nil)
+        
+        SocketService.instance.getChatMessage { (success) in
+            if success{
+                self.tableView.reloadData()
+                if MessageService.instance.messages.count>0{
+                    let endIndex = IndexPath(row: MessageService.instance.messages.count-1, section: 0)
+                    self.tableView.scrollToRow(at: endIndex, at:.bottom, animated: true)
+                }
+            }
+        }
+        
         if AuthService.instance.isLoggedIn{
             AuthService.instance.findUserByEmail(completion: { (success) in
                // print("-- token --")
@@ -58,6 +73,7 @@ class ChatVC: UIViewController {
             onLoginGetMessages()
         }else{
             channelNameLbl.text = "Please Log In"
+            tableView.reloadData()
         }
     }
     
@@ -66,6 +82,20 @@ class ChatVC: UIViewController {
         channelNameLbl.text = "#\(channelName)"
         getMessages()
     }
+    
+    
+    @IBAction func messageBoxEditing(_ sender: Any) {
+        if messageTxt.text == ""{
+            isTyping = false
+            sendBtn.isHidden = true
+        }else{
+            if isTyping == false{
+                sendBtn.isHidden = false
+            }
+            isTyping = true
+        }
+    }
+    
     @IBAction func sendMessagePressed(_ sender: Any) {
         if AuthService.instance.isLoggedIn{
             guard let channelId = MessageService.instance.selectedChannel?.id else {return}
--- Smack/Services/UserDataService.swift
+++ Smack/Services/UserDataService.swift
@@ -85,6 +85,7 @@ class UserDataService {
         AuthService.instance.userEmail = ""
         AuthService.instance.authToken = ""
         MessageService.instance.clearChannels()
+        MessageService.instance.clearMessages()
     }
     
 }
